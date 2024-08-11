a2d63cfbcfb512e4595ed29cab1538c19d9fe76b
Swaroop Raghupathy <54129659+swaroopar@users.noreply.github.com>
2024-08-11 07:51:31 +0200

    dependencies: bump @typescript-eslint/eslint-plugin from 7.15.0 to 8.0.1 (#142)
    * dependencies: bump @typescript-eslint/eslint-plugin from 7.15.0 to 8.0.1
    
    * dependencies: bump @typescript-eslint/eslint-plugin from 7.15.0 to 8.0.1

--- plugins/tailwind-plugin.ts
+++ plugins/tailwind-plugin.ts
@@ -1,10 +1,13 @@
 import { PostCssOptions } from '@docusaurus/types';
+import postcssImport from 'postcss-import';
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
 
 export function tailwindPlugin() {
     return {
         name: 'tailwind-plugin',
         configurePostCss(postcssOptions: PostCssOptions) {
-            postcssOptions.plugins = [require('postcss-import'), require('tailwindcss'), require('autoprefixer')];
+            postcssOptions.plugins = [postcssImport(), tailwindcss(), autoprefixer()];
             return postcssOptions;
         },
     };
