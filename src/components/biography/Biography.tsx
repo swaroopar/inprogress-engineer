7f599280cdb25d27881ad9f84cecb3ddeaa0f0b2
Swaroop AR <swaroop.ar@outlook.com>
2026-08-01 21:59:37 +0200

    extend LLM architecture

--- src/components/biography/Biography.tsx
+++ src/components/biography/Biography.tsx
@@ -6,10 +6,10 @@ import Image from '@theme/IdealImage';
 export function Biography(): React.JSX.Element {
     return (
         <div className='container flex flex-col justify-items-center p-7 md:mb-4 md:flex md:flex-row md:overflow-hidden md:p-20'>
-            <div className='text-center'>
+            <div className=''>
                 <h3 className='font-sans'>Introduction</h3>
                 <br />
-                <BiographyAd />
+                <BiographyAd className='text-center' />
             </div>
             <div className='md:pt-20'>
                 <Image className='' img={avatar as string}></Image>
