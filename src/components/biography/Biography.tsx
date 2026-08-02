cd4c13ce951eb508b11e0fc05314b025ef172877
Swaroop AR <swaroop.ar@outlook.com>
2026-08-02 19:50:22 +0200

    fix main page structure

--- src/components/biography/Biography.tsx
+++ src/components/biography/Biography.tsx
@@ -5,14 +5,16 @@ import Image from '@theme/IdealImage';
 
 export function Biography(): React.JSX.Element {
     return (
-        <div className='container flex flex-col justify-items-center p-7 md:mb-4 md:flex md:flex-row md:overflow-hidden md:p-20'>
-            <div className=''>
-                <h3 className='font-sans'>Introduction</h3>
-                <br />
-                <BiographyAd className='text-center' />
-            </div>
-            <div className='md:pt-20'>
-                <Image className='' img={avatar as string}></Image>
+        <div className='container p-7 md:mb-4 md:overflow-hidden md:p-20'>
+            <h3 className='font-sans'>Introduction</h3>
+            <br />
+            <div className='flex flex-col justify-items-center md:flex md:flex-row md:items-center'>
+                <div className=''>
+                    <BiographyAd />
+                </div>
+                <div className='flex-shrink-0 flex justify-center md:justify-end'>
+                    <Image className='w-64 h-64 md:w-80 md:h-80' img={avatar as string}></Image>
+                </div>
             </div>
         </div>
     );
