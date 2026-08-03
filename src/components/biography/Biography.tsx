2f069232553cea6ad0624e846f2dc562feb343b0
Swaroop Raghupathy <54129659+swaroopar@users.noreply.github.com>
2026-08-03 09:37:32 +0200

    extend LLM architecture (#814)
    * extend LLM architecture
    
    * fix diagrams
    
    * fix main page structure
    
    * fix main page structure

--- src/components/biography/Biography.tsx
+++ src/components/biography/Biography.tsx
@@ -5,14 +5,16 @@ import Image from '@theme/IdealImage';
 
 export function Biography(): React.JSX.Element {
     return (
-        <div className='container flex flex-col justify-items-center p-7 md:mb-4 md:flex md:flex-row md:overflow-hidden md:p-20'>
-            <div className='text-center'>
-                <h3 className='font-sans'>Introduction</h3>
-                <br />
-                <BiographyAd />
-            </div>
-            <div className='md:pt-20'>
-                <Image className='' img={avatar as string}></Image>
+        <div className='container p-7 md:mb-4 md:overflow-hidden md:p-20'>
+            <h3 className='font-sans'>Introduction</h3>
+            <br />
+            <div className='flex flex-col justify-items-center md:flex md:flex-row md:items-center'>
+                <div className=''>
+                    <BiographyAd />
+                </div>
+                <div className='flex flex-shrink-0 justify-center md:justify-end'>
+                    <Image className='h-64 w-64 md:h-80 md:w-80' img={avatar as string}></Image>
+                </div>
             </div>
         </div>
     );
